9d575ea71ecdfd31488162649601c155f29162de
sielbien7 <cnahueldasilva@gmail.com>
2022-05-16 21:47:28 -0300

    Normalizar info: se agregaron funciones que obtienen el mes mas reciente, obtienen el archivo mas reciente, y una funcion que crea una tabla con las columnas correspondientes. Main: tests de escritura de datos sobre la tabla normalizada. Falta crear funciones y moverlo al archivo normalizarinfo.py

--- normalizarinfo.py
+++ normalizarinfo.py
@@ -19,21 +19,21 @@ def get_latest_month (categoria):
     mes=''
     dir_list = os.listdir(path)
     all_folders = ''
-    all_folders = all_folders.join(dir_list) #uno todas las carpetas en un string
+    all_folders = all_folders.join(dir_list) #une nombres de carpetas en un solo string
     for i in range(len(dir_list)):
         for x in range(12, 0, -1):
-            if (all_folders.rfind(meses.get(x))) != -1: #me fijo que mes se encuentra en las carpetas empezando desde diciembre
+            if (all_folders.rfind(meses.get(x))) != -1: #revisa que mes encuentra en la string, empezando por el mes 12.
                 mes = meses.get(x)
                 return mes;
 
 def get_latest_file(categoria):
     """obtiene ruta del archivo mas reciente de la carpeta"""
-    ruta='M:/Documents/2022/pythonProjects/ProyectoCool/Data/'+categoria+'/2022-'+get_latest_month(categoria) #creo ruta segun el ultimo mes que se encuentra
+    ruta='M:/Documents/2022/pythonProjects/ProyectoCool/Data/'+categoria+'/2022-'+get_latest_month(categoria) #crea ruta segun el ultimo mes que se encuentra
     tipo_archivo=r'/*csv'
     print(ruta)
     archivos=glob.glob(ruta+tipo_archivo)
     ultimo_archivo=max(archivos,key=os.path.getctime)
-    return ultimo_archivo #devuelvo ruta del archivo mas reciente de la carpeta
+    return ultimo_archivo #devuelve ruta del archivo mas reciente de la carpeta con el mes mas reciente
 
 def crear_tabla_normalizada():
     """funcion que crea la tabla que contendrá la información normalizada"""
